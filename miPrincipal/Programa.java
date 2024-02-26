e7ac99a17c8a794264d01dee54c218575ca087d1
Zahid Rene Jimenez Olivas <159456299+REHID-35@users.noreply.github.com>
2024-02-26 15:06:25 +0000

    Actualizacion 26/02/2024

---
+++ miPrincipal/Programa.java
@@ -0,0 +1,20 @@
+package miPrincipal;
+import otrosprogramas.*;//esta sentencia importa toda las clases del paquete otrosprogramas 
+public class Programa {
+    public static void main(String[] args) {
+        obtenerDato();
+        alfabetizar();
+        verpalabras();
+    }
+    public static void obtenerDato(){
+        System.out.println("Estoy en la funcion obtener datos");
+    }
+    public static void alfabetizar(){
+        System.out.println("Estoy en la funcion alfabetizar");
+
+    }
+    public static void verpalabras(){
+        System.out.println("Estoy en la funcion ver palabras");
+        
+    }
+}
---
+++ otrosprogramas/programa.java
@@ -0,0 +1,13 @@
+package otrosprogramas;
+
+public class programa {
+    public static void obtenerDato() {
+        System.out.println("Estoy en obtener datos del paquete otrosprogramas");
+    }
+    public static void alfabetizar() {
+        System.out.println("Estoy en alfabetizar del paquete otrosprogramas");
+    }
+    public static void verpalabras(){
+        System.out.println("Estoy en ver palabras del paquete otrosprogramas");
+    }
+}
