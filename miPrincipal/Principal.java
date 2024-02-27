19408c6b911bf4dff599805fcacb0c2104e02274
Zahid Rene Jimenez Olivas <159456299+REHID-35@users.noreply.github.com>
2024-02-27 15:04:14 +0000

    actulizacion 27/02/2024

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -6,5 +6,18 @@ public class Principal {
     
     public static void main(String[] args) {
         System.out.println("Hola Mundo");//comentario
+        //demostracion tipo de variale
+        /*los tipo de varibale float y double tiene 3 variables */
+        /*infinito positivo */
+        /*infinito negativo*/
+        /*Nah, I'd win */
+        /*Nan (not number) */
+
+        System.out.println(Math.sqrt(8));//retorno un punto flotante
+        System.out.println(Math.sqrt(-3));//NaN
+        System.out.println(1.1e5*1.1e2);
+        System.out.println(1.10000*1.10);//retoran infinito pos
+        System.out.println(1.1e200*1.1e200);//da infinito
+        
     }
 }
